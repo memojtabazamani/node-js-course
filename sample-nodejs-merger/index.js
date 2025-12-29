06da8ee619ce7ddcd89432614d5d15dac8e5f154
mojtaba zamani <memojtabazamani@gmail.com>
2025-12-29 21:34:08 +0330

    node-js-course
    work with papaparse and read csv files

--- sample-nodejs-merger/index.js
+++ sample-nodejs-merger/index.js
@@ -1,8 +1,10 @@
-console.log('Running')
-
-const newHandler = (p1) => {
-  if (p1 === 'Mojtaba') return true
-  else return false
+// 1. Read CSV files
+const { readCSV } = require('./src/utils/fileHandler')
+const readFiles = async () => {
+  const json = await readCSV('./input/catalogA.csv')
+  console.log('json', json)
 }
+readFiles()
+// 2. Process Files and Merfe Products
 
-newHandler()
+// 3. Generate Output file
