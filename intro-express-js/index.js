efa49fd324feacc9729ae9a314aef5cc49e4d0df
Mojtaba zamani <memojtabazamani@gmail.com>
2025-12-30 12:56:33 +0330

    start with express s

---
+++ intro-express-js/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1> Hello World! </h1>");
+});
+app.get("/about", (req, res) => {
+  res.send("<h1> About page </h1>");
+});
+app.get("/contact", (req, res) => {
+  res.send("<h1> Contact page </h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
